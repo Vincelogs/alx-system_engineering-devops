31de825f6dfc4a58ccc5d1fce4171a251078b101
vincelogs <vincentharyor@gmail.com>
2023-04-25 05:14:03 -0700

    Python script that, using this REST API, for a given employee ID, returns information about his/her TODO list progress

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -11,7 +11,7 @@ if __name__ == '__main__':
     user_id = int(argv[1])
 
     # Send a GET request to the API to get the user's name
-    user_name = rq.get(f"{url}/users/{user_id}").json()["name"]
+    EMPLOYEE_NAME = rq.get(f"{url}/users/{user_id}").json()["name"]
 
     # Send a GET request to the API to get the user's TODO_list
     todos = rq.get(f"{url}/todos?userID={user_id}").json()
@@ -23,12 +23,12 @@ if __name__ == '__main__':
             user_todos.append(todo)
 
     # Calculate the number of completed and total tasks
-    total_tasks = len(user_todos)
-    completed_tasks = len([todo for todo in user_todos if todo["completed"]])
+    TOTAL_NUMBER_OF_TASKS = len(user_todos)
+    NUMBER_OF_DONE_TASKS = len([todo for todo in user_todos if todo["completed"]])
 
     # Print the information about the user's TODO list progress
     print('Employee {} is done with tasks({}/{}):'
-          .format(user_name, completed_tasks, total_tasks))
+          .format(EMPLOYEE_NAME, NUMBER_OF_DONE_TASKS, TOTAL_NUMBER_OF_TASKS))
     for todo in user_todos:
         if todo["completed"]:
             print(f"\t{todo['title']}")
