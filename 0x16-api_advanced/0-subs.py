6fe4b29267a1b5cd5393b313e157787ec06c3920
vincelogs <vincentharyor@gmail.com>
2023-05-09 15:54:33 -0700

    queries the Reddit API and returns the number of subscribers

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+'''queries and returns corresponding result from the Reddit API'''
+import requests
+
+
+def number_of_subscribers(subreddit):
+    '''
+    Description:
+    - queries and returns the number of subscibers
+      (not active users, total subscribers) for a given subreddit.
+    - returns 0 if an invalid subreddit is given
+    '''
+    # Set a custom user agent to avoid Too Many Requests error
+    headers = {'User-Agent': 'My User-Agent 0.1'}
+    endpoint = f'https://www.reddit.com/r/{subreddit}/about.json'
+    response = requests.get(endpoint, headers=headers)
+
+    if response.status_code == 200:
+        data = response.json()['data']
+        return data['subscribers']
+    else:
+        return 0
